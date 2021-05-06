7c087c0eb02aefb8c33a4010db8955e55be8fd8f
Lukas Lysychas <m80silver@gmail.com>
2021-05-06 19:03:41 +0300

    Switch to Heroku host, use process.env

--- server.js
+++ server.js
@@ -37,23 +37,23 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`);
 });
 
-// Create a function to terminate your app gracefully:
-function gracefulShutdown() {
-  // First argument is [force], see mongoose doc.
-  client.close(() => {
-    console.log('MongoDb connection closed.');
-  });
-}
+// // Create a function to terminate your app gracefully:
+// function gracefulShutdown() {
+//   // First argument is [force], see mongoose doc.
+//   client.close(() => {
+//     console.log('MongoDb connection closed.');
+//   });
+// }
 
-// Ask node to run your function before exit:
+// // Ask node to run your function before exit:
 
-// This will handle process.exit():
-process.on('exit', gracefulShutdown);
+// // This will handle process.exit():
+// process.on('exit', gracefulShutdown);
 
-// This will handle kill commands, such as CTRL+C:
-process.on('SIGINT', gracefulShutdown);
-process.on('SIGTERM', gracefulShutdown);
-process.on('SIGKILL', gracefulShutdown);
+// // This will handle kill commands, such as CTRL+C:
+// process.on('SIGINT', gracefulShutdown);
+// process.on('SIGTERM', gracefulShutdown);
+// process.on('SIGKILL', gracefulShutdown);
 
-// This will prevent dirty exit on code-fault crashes:
-process.on('uncaughtException', gracefulShutdown);
+// // This will prevent dirty exit on code-fault crashes:
+// process.on('uncaughtException', gracefulShutdown);
